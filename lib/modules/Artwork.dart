9c6b09c9c1248f28223cf2253770b2da7192d4d9
Makiaaa <baliislam99@gmail.com>
2022-06-15 03:35:05 +0100

    this is our application

---
+++ lib/modules/Artwork.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Artwork {
+  late String name;
+  late String description;
+  late String imageUrl;
+
+  Artwork(
+      {required this.name, required this.description, required this.imageUrl});
+}
---
+++ lib/modules/Gods.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Gods {
+  late String name;
+  late String description;
+  late String imageUrl;
+
+  Gods(
+      {required this.name, required this.description, required this.imageUrl});
+}
